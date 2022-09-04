9d6042537a14316f824f3ac733a32f443150d39d
Michael Terra <michaelterra@gmail.com>
2022-09-04 17:52:32 -0300

    classe de modelo

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -18,7 +18,8 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
-import { ProductCreateComponent } from './components/product-create/product-create.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+
 @NgModule({
   declarations: [
     AppComponent,
